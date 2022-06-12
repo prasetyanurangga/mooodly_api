4df1de278e63f859d7266e2cae0956f79259b44d
prasetyanurangga <angganurprasetya4@gmail.com>
2022-06-13 02:09:45 +0700

    🎉

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"github.com/prasetyanurangga/snaptify_api/spotify"
     "encoding/json"
     "github.com/gin-gonic/gin"
-    "github.com/gin-contrib/cors"
     "os"
     "strings"
     "fmt"
@@ -326,7 +325,7 @@ func main() {
  //        AllowCredentials: true,
  //        AllowHeaders: []string{"Access-Control-Allow-Headers", "Content-Type", "Content-Length", "Accept-Encoding", "X-CSRF-Token", "Authorization", "accept", "origin", "Cache-Control", "X-Requested-With"},
  //    }))
- 
+
 	router.Use(CORSMiddleware())
     router.GET("/get_spotify", func(c *gin.Context) {
         accessToken := c.GetHeader("access_token")
